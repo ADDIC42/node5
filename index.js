eb7f680861b8d4df31a1358d25cafcee665a9bd0
Адам Саралиев <adamfuturama@mail.ru>
2022-11-07 23:13:52 +0300

    nervi sdayt

---
+++ index.js
@@ -0,0 +1,10 @@
+let figlet = require('figlet');
+
+figlet('Inctocode', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
